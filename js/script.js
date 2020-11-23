d9fc58956c58e868a770139e1afb1b2df8afc386
proffpersona <73022171+proffpersona@users.noreply.github.com>
2020-11-23 13:16:56 +0300

    lawyer1

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 480){
+      $('#topBtn').fadeIn();
+    } else{
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function(){
+    $('html ,body').animate({scrollTop : 0},800);
+  });
+});
+
+
+$("a.nav-link").on("click", function(e){
+  e.preventDefault();
+  var anchor = $(this).attr('href');
+  $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top - 60
+  }, 900);
+});
+
+
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
